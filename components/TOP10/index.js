eeb7790e318da90693c3a40ec6e1058c55a49b88
Taryana <hatakekakasih2002@gmail.com>
2022-10-19 21:04:21 +0700

    added search and detailFlight page

--- components/TOP10/index.js
+++ components/TOP10/index.js
@@ -2,11 +2,22 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 import styles from './top.module.css';
 import axios from 'axios';
+import btnPrev from '../../assets/logo/logo-prev.svg'
+import btnback from '../../assets/logo/logo-back.svg'
 import { useDispatch, useSelector } from 'react-redux';
 import { DestinationAction } from '../../config/redux/actions/destinationAction'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+// import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function TopTen({src, title}) {
+  const swiper = useSwiper();
     const dispatch = useDispatch();
     const datas = useSelector((state) => state.destination);
     console.log(datas)
@@ -21,15 +32,46 @@ function TopTen({src, title}) {
       <p className='fs-3 fw-bold'>Top 10 destinations</p>
     </div>
     <div className={styles["topten-img"]}>
+    <Swiper
+        slidesPerView={5}
+        spaceBetween={30}
+        slidesPerGroup={2}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        modules={[Pagination, Navigation]}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        navigation={true}
+        // navigation= {{
+        //   nextEl: '.swiper-button-next',
+        //   prevEl: '.swiper-button-prev',
+        // }}
+        className="mySwiper"
+      >
         {datas?.data?.map((item, index) => (
-            <div className={`mt-4`} key={index}>
-                <div className={`${styles.container_img}`}>
-                    <Image src={src} alt="" className={`${styles.img}`}/>
-                </div>
-                <span>{item.city}</span>
-            </div>
+          <>
+            <SwiperSlide>
+              <div className={`mt-4`} key={index}>
+                  <div className={`${styles.container_img}`}>
+                      <Image src={src} alt="" className={`${styles.img}`}/>
+                  </div>
+                  <span>{item.airport_arrive_city}</span>
+              </div>
+            </SwiperSlide>
+          </>
             ))
         }
+            {/* <div className="boxOfBtn w-100">
+            <button className="btnNextBlue swiper-button-prev">
+              <Image src={btnback} alt="" className="btnImg" />
+            </button>
+            <button onClick={() => swiper.slideNext()}>Slide to the next slide</button>
+            <button className="btnPrevious swiper-button-next">
+              <Image src={btnPrev} alt="" />
+            </button>
+          </div> */}
+      </Swiper>
     </div>
 
   </div>
--- config/redux/actions/airlineAction.jsx
+++ config/redux/actions/airlineAction.jsx
@@ -9,7 +9,7 @@ export const AirlineAction = (state = initialState) => async(dispatch) =>{
         const result = await axios.get(`${process.env.API_BACKEND}flight?search=${state.search}`)
         const data = result.data
         console.log(data)
-        dispatch({type: "GET_AIRLINES", ...state, payload: data})
+        dispatch({ type: "GET_AIRLINES", ...state, payload: data } )
     } catch (error) {
         console.log(error)
     }
